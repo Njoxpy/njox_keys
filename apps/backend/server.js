8667822214777fa69330aa698a086ea77a2fe9ba
Njox Njox <godblessnyagawa@gmail.com>
2025-03-03 15:01:36 +0300

    feat: add more routes for order and stats

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -1,27 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const compression = require("compression");
 require("dotenv").config();
 const connectDB = require("./config/dbConfig");
-const redis = require("redis");
 
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Support JSON requests
+app.use(express.json());
+app.use(compression());
 
 // routes export
 const venueRoutes = require("./routes/venue.routes");
 const statsRoutes = require("./routes/stats.routes");
+const statusRouter = require("./routes/status.routes");
+const orderRoutes = require("./routes/orders.routes");
 
 // register routes
 app.use("/api/v1/venues", venueRoutes);
 
 // stats
 app.use("/api/v1/stats", statsRoutes);
+app.use("/api/v1/status", statusRouter);
+app.use("/api/v1/orders", orderRoutes);
 
-// reddis
-console.log(redis);
 // not found route
 app.use("*", (req, res) => {
   res.status(404).json({ message: "Route not found!" });
@@ -33,7 +36,7 @@ const startServer = async () => {
 
     const port = process.env.PORT || 3000;
     app.listen(port, () => {
-      console.log(`🚀 Server running on port ${port}`);
+      console.log(`🚀 Server running on port http://localhost:${port}`);
       console.log(`🌍 Environment: ${process.env.NODE_ENV || "development"}`);
       console.log(
         `📊 Database: ${process.env.MONGO_URI ? "Connected" : "Not Configured"}`
