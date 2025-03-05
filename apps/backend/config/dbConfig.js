1b418852ea60fa177852b165d91f35500cd140b1
Njox Njox <godblessnyagawa@gmail.com>
2025-03-05 07:38:00 +0300

    fix: add check for mongouri if does not exist exit.

--- apps/backend/config/dbConfig.js
+++ apps/backend/config/dbConfig.js
@@ -2,6 +2,12 @@ const mongose = require("mongoose");
 
 const connectDB = async () => {
   try {
+
+    if (!process.env.MONGO_URI) {
+      console.log("There is nto env variable for mongo uri...")
+
+      process.exit(1)
+    }
     mongose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.error("failed to connect to database", error.message);
