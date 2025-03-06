e0266dab5fdb64d4383f7996c22c742fa566cb83
Njox Njox <godblessnyagawa@gmail.com>
2025-03-06 20:01:54 +0300

    feat: add indexing and venue number for the venue.

--- apps/backend/models/venue.model.js
+++ apps/backend/models/venue.model.js
@@ -5,32 +5,47 @@ const venueSchema = new mongoose.Schema(
       type: String,
       required: true,
       maxLength: 20,
+      trim: true,
     },
     block: {
       type: String,
       required: true,
       maxLength: 100,
+      index: true,
+      trim: true,
     },
     capacity: {
       type: Number,
       required: true,
       min: 0,
+      index: true,
+    },
+    venueNumber: {
+      type: Number,
+      required: [true, "Venue number is required!"],
+      unique: [true, "Venue number is alredy in use."],
+      index: true,
     },
     description: {
       type: String,
       required: true,
       maxLength: 500,
+      trim: true,
     },
     equipment: [
       {
         type: String,
         required: true,
+        trim: true,
       },
     ],
     images: [
       {
         type: String,
-        //   required: true,
+        default: [
+          "https://images.pexels.com/photos/101808/pexels-photo-101808.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        ],
+        required: true,
       },
     ],
     name: {
@@ -39,12 +54,20 @@ const venueSchema = new mongoose.Schema(
       unique: true,
       minLength: 2,
       trim: true,
+      trim: true,
     },
     status: {
       type: String,
       enum: ["available", "booked"],
       default: "available",
       required: true,
+      index: true,
+      trim: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User ID is required"],
     },
   },
   { timestamps: true }
